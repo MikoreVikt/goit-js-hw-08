8ffc8d89dd0b2e950c7e43dcef8878346b9bae00
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-10-20 02:35:31 +0300

    adds first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainerRef.innerHTML = galleryMarkup;
+
+galleryContainerRef.addEventListener(`click`, event => event.preventDefault());
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img loading="lazy" class="gallery__image lazyload"  data-source="${original}" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  docClose: true,
+});
