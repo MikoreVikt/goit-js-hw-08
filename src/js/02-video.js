7a27bd86558074a3c53a5a8f4606eb66ab67b6a9
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-10-20 23:18:23 +0300

    adds second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+returnCarrentTime();
+
+player.on(`timeupdate`, throttle(currentTime, 1000));
+
+function currentTime(data) {
+  localStorage.setItem(LOCALSTORAGE_KEY, `${data.seconds}`);
+}
+
+function returnCarrentTime() {
+  const saveTimeToPlay = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (saveTimeToPlay) {
+    player.setCurrentTime(saveTimeToPlay);
+  }
+}
